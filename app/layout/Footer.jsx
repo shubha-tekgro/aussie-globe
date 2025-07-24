e11f924c243a39900e98554efaef54c0435b3473
utsavtekgro <utsav@tekgro.au>
2025-07-24 11:49:43 +0100

    Create footer responsive

--- app/layout/Footer.jsx
+++ app/layout/Footer.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import cebrabian_right from '../icons/cebrabian_right.svg';
+import cebrabian_down1 from '../icons/cebrabian_down1.svg';
 import logo from '../icons/logo.svg';
 import facebook from '../icons/facebook.svg';
 import twitter from '../icons/twitter.svg';
@@ -11,29 +12,28 @@ import aboriginal from '../icons/aboriginal.svg';
 
 export default function Footer() {
   return (
-    <section className='mt-96 bg-slate-100 '>
-      <div className='px-4 py-10 bg-gradient-to-b from-blue to-blue-500 text-white mx-auto flex flex-col gap-6 text-center sm:px-6 md:px-8 2xl:ml-64 2xl:mr-32 '>
-
-        <div className='flex flex-col lg:flex-row justify-between items-start gap-6'>
-          <h2 className='-ml-20  2xl:text-2xl text-xl font-semibold'>
-            Unlock Your Future With Our Fast & Trusted Processing
-          </h2>
-
-          <button className='bg-red text-white px-4 sm:px-6 py-3 rounded flex items-center gap-2 text-sm self-start lg:self-center hover:bg-red-600'>
-            Get a Free Consultation
-            <Image src={cebrabian_right} alt='cebrabian right' className='w-4 h-4' />
-          </button>
-        </div>
-
-        <div className='flex flex-col lg:flex-row justify-between items-start gap-6'>
-          <p className='text-base sm:text-lg md:text-xl opacity-80 leading-relaxed max-w-2xl'>
-            Get expert guidance for admissions, visa, and career success — hassle-free!</p>
-          <button className=' px-11 py-3 rounded flex items-center border-2 bg-blue-500 text-whit hover:border-blue-500 
-            text-sm self-start lg:self-center'> Talk to Our Experts!
-          </button>
+    <section className='mt-96 '>
+      <div className=' 2xl:py-10  2xl:px-8 py-10  bg-gradient-to-b from-blue to-blue-500 text-white 2xl:ml-64 2xl:mr-32'>
+        <div className='flex flex-col lg:flex-row justify-between items-center gap-8'>
+          <div className='flex flex-col ml-4 items-center lg:items-start text-center lg:text-left gap-4'>
+            <h2 className='text-2xl sm:text-3xl font-bold '>
+              Unlock Your Future with Our Fast & Trusted Processing
+            </h2>
+            <p className='text-base sm:text-lg opacity-80'>
+              Get expert guidance for admissions, visas, and career success—hassle-free!
+            </p>
+          </div>
+          <div className='flex flex-col items-center gap-4 sm:gap-4 2xl:mr-16'>
+            <button className='bg-red hover:bg-red-600 text-white px-6 py-3 rounded flex items-center gap-4 w-full sm:w-auto sm:px-12 sm:py-3 2xl:w-full'>
+              Get a Free Consultation
+              <Image src={cebrabian_right} alt='cebrabian-right' />
+            </button>
+            <button className='bg-blue-500 hover:border-blue-500 text-white border-2 px-12 py-3 rounded flex items-center w-full sm:w-auto 2xl:w-full'>
+              Talk to Our Experts!
+            </button>
+          </div>
         </div>
       </div>
-
       <div className='flex flex-col md:flex-row flex-wrap justify-start px-16 md:px-20 lg:px-40 mt-4 '>
         <div className='px-4 md:px-12 lg:px-24 ml-12 '>
           <Image src={logo} alt='Logo' className='w-32 my-2' />
@@ -41,9 +41,14 @@ export default function Footer() {
             Trusted experts in education & migration, helping students and professionals achieve their Australian dream with fast, reliable, and hassle-free solutions.
           </div>
         </div>
+
+
+
         <div className='flex flex-col w-full sm:w-auto md:ml-8'>
           <h2 className='text-sub mb-4 '>New to Aussie Globe?</h2>
           <nav className='flex flex-col gap-2 text-sm text-gray'>
+          <Image src={cebrabian_down1} alt='Logo' className='w-4 my-2 hidden ' />
+
             <div>Who We Are</div>
             <div>Who We Offer</div>
             <div>Why Choose Us</div>
